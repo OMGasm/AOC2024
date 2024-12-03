19245a85be0a5958bd399a9a189bd53478172e72
OMGasm <OMGasm@users.noreply.github.com>
2024-12-04 05:19:19 +1000

    some tests

---
+++ challenges/lib/day2.ex
@@ -0,0 +1,29 @@
+defmodule Day2 do
+  def solve_from_file(filename, part) do
+    {:ok, 0}
+  end
+
+  @doc """
+  ## Examples
+  iex>  Day2.is_line_safe? [7, 6, 4, 2, 1]
+  true
+
+  iex>  Day2.is_line_safe? [1, 2, 7, 8, 9]
+  false
+
+  iex>  Day2.is_line_safe? [9, 7, 6, 2, 1]
+  false
+
+  iex>  Day2.is_line_safe? [1, 3, 2, 4, 5]
+  false
+
+  iex>  Day2.is_line_safe? [8, 6, 4, 4, 1]
+  false
+
+  iex>  Day2.is_line_safe? [1, 3, 6, 7, 9]
+  true
+  """
+  def is_line_safe? list do
+    true
+  end
+end
--- challenges/test/simple_test.exs
+++ challenges/test/simple_test.exs
@@ -10,4 +10,10 @@ defmodule Day1Test do
     assert Day1.solve_from_file("inputs/day1_example", :part2) == {:ok, 31}
   end
 
+  doctest Day2
+
+  test "day 2 part 1" do
+    assert Day2.solve_from_file("inputs/day2_example", :part1) == {:ok, 2}
+  end
+
 end
